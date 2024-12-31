5218cdf2c6f6194b91e9c800b0fefaeb7ebc0fac
prasanth <prasanth@zohocorp.com>
2024-12-31 18:31:16 +0530

    changed to graphql for adding movies into watchlist

--- import_watchlist.py
+++ import_watchlist.py
@@ -1,64 +1,145 @@
-import requests
-import urllib.parse
+import http.client
 import json
 import csv
 from time import sleep
 
 headers = {
-    'authority': 'apis.justwatch.com',
-    'accept': 'application/json, text/plain, */*',
-    'origin': 'https://www.justwatch.com',
-    'referer': 'https://www.justwatch.com/in/watchlist',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41'
+   'User-Agent': 'insomnia/10.0.0',
+   'Content-Type': 'application/json'
 }
+
+search_query = """
+    query GetSearchTitles(
+        $country: Country!
+        $language: Language!
+        $searchTitlesFilter: TitleFilter
+        $searchTitlesSortBy: PopularTitlesSorting! = POPULAR
+    ) {
+        popularTitles(
+            first: 1
+            country: $country
+            filter: $searchTitlesFilter
+            sortBy: $searchTitlesSortBy
+        ) {
+            edges {
+            ...SearchTitleGraphql
+            }
+        }
+    }
+    fragment SearchTitleGraphql on PopularTitlesEdge {
+        node {
+            id
+            content(country: $country, language: $language) {
+                title
+            }
+        }
+    }
+"""
+
+add_to_watchlist_query = """
+    mutation SetInWatchlist($input: SetInTitleListInput!) {
+        setInWatchlistV2(input: $input) {
+            title {
+                id
+            }
+        }
+    }
+
+"""
+
+search_variables = {
+    "searchTitlesSortBy": "POPULAR",
+    "searchTitlesFilter": {
+        "objectTypes": [
+            ""
+        ],
+        "excludeIrrelevantTitles": False,
+        "includeTitlesWithoutUrl": True,
+        "releaseYear": {},
+        "searchQuery": ""
+    },
+    "language": "en",
+    "country": "IN"
+}
+
+add_to_watchlist_variables = {
+	"input": {
+		"id": "",
+		"state": True
+	}
+}
+
+def get_id(title, type, year):
+    search_filter = search_variables["searchTitlesFilter"]
+    search_filter["searchQuery"] = title
+    search_filter["releaseYear"] = {
+        "min": year
+    }
+    search_filter["objectTypes"] = type
+    search_variables["searchTitlesFilter"] = search_filter
+    
+    payload = {
+        "query": search_query,
+        "variables": search_variables
+    }
+
+    conn = http.client.HTTPSConnection("apis.justwatch.com")
+    try:
+        payload_json = json.dumps(payload)
+        conn.request("POST", "/graphql", payload_json, headers)
+        response = conn.getresponse()
+        response_str = response.read().decode()
+        json_response = json.loads(response_str)
+        return json_response["data"]["popularTitles"]["edges"][0]["node"]["id"]
+    except Exception as e:
+        print("The title " + title + " could not be found in the justwatch website. So skipping it...")
+    finally:
+        conn.close()
+
+def add_to_watchlist(id):
+    token = '<your_token>'
+    headers["Authorization"] = "Bearer " + token
+    conn = http.client.HTTPSConnection("apis.justwatch.com")
+    try:
+        add_to_watchlist_variables["input"]["id"] = id
+        payload = {
+            "query": add_to_watchlist_query,
+            "variables": add_to_watchlist_variables
+        }
+        payload_json = json.dumps(payload)
+        conn.request("POST", "/graphql", payload_json, headers)
+        response = conn.getresponse()
+        print(response.read().decode())
+        return (response.getcode() == 200)
+    except Exception as e:
+        return
+    finally:
+        conn.close()
+        del headers["Authorization"]
 
 def get_movies_list():
     csv_filename = 'WATCHLIST.csv'
-    movies_list = list()
     with open(csv_filename, 'r', encoding = 'ISO-8859-1') as f:
         csv_reader = csv.reader(f, delimiter=',')
-        line_count = 0
+        next(csv_reader, None)
         for row in csv_reader:
-            if line_count > 0:
-                line_count += 1
-                movie_with_year = row[5] + ' ' + row[10]
-                movies_list.append(movie_with_year)
+            title, type, year = row[6], row[8], row[11]
+            if(type.endswith("Movie")):
+                type = "MOVIE"
+            elif(type.endswith("Series")):
+                type = "SHOW"
+            id = get_id(title, type, year)
+            if(id is None):
+                continue
+            print(title, id)
+            
+            if(add_to_watchlist(id)):
+                print('added ' + title + ' to your watchlist')
             else:
-                line_count += 1
-    return movies_list
-
-def add_to_watchlist(obj_id, obj_type):
-    justwatch_id = ''
-    authorization = ''
-    url = ('https://apis.justwatch.com/personalization/title_list/watchlist/object_type/{0}/object_id/{1}?justwatch_id={2}').format(obj_type, obj_id, justwatch_id)
-    if 'authorization' not in headers:
-        headers['authorization'] = authorization
-    requests.put(url, headers=headers)
+                print("Encountered a problem while adding " + title)
+            sleep(1)
 
 if __name__ == '__main__':
-    print('importing in progress...')
-    url = 'https://apis.justwatch.com/content/titles/en_IN/popular?body='
-    query_param = {
-        'page_size': 1,
-        'content_types': [
-            'show',
-            'movie'
-        ]
-    }
-    movies_list = get_movies_list()
-    for movie in movies_list:
-        query_param['query'] = movie
-        request_url = url + urllib.parse.quote(json.dumps(query_param))
-        try:
-            response = requests.get(request_url, headers=headers)
-            json_response = response.json()
-            object_id = json_response["items"][0]["id"]
-            object_type = json_response["items"][0]["object_type"]
-            add_to_watchlist(object_id, object_type)
-        except Exception as e:
-            print("The title " + movie + " could not be found in the justwatch website. So skipping it...")
-            continue
-        print('added ' + movie + ' to your watchlist')
-        sleep(3)
-    print('finished importing movies and shows to your justwatch watchlist')
+    print('Started...')
+    get_movies_list()
+    print('Imported movies and shows into your justwatch watchlist')
